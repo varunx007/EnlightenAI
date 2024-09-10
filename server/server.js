4516f6221094d6bea8ac30636d61ee3c0c2a77e4
Varun Reddy <136375960+varunx007@users.noreply.github.com>
2024-09-10 22:04:24 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -14,6 +14,8 @@ const { createApi } = require('unsplash-js');
 const showdown = require('showdown');
 const axios = require('axios');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Flutterwave = require('flutterwave-node-v3');
+const flw = new Flutterwave(process.env.FLUTTERWAVE_PUBLIC_KEY, process.env.FLUTTERWAVE_SECRET_KEY);
 
 //INITIALIZE
 const app = express();
@@ -129,13 +131,45 @@ app.post('/api/signin', async (req, res) => {
         }
 
         if (password === user.password) {
-            return res.json({ success: true, message: 'SignIn successful', userData: user });
+            return res.json({ success: true, message: 'SignIn Successful', userData: user });
         }
 
         res.json({ success: false, message: 'Invalid email or password' });
 
     } catch (error) {
         res.status(500).json({ success: false, message: 'Invalid email or password' });
+    }
+
+});
+
+//SIGNINSOCIAL
+app.post('/api/social', async (req, res) => {
+    const { email, name } = req.body;
+    let mName = name;
+    let password = '';
+    let type = 'free';
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            const estimate = await User.estimatedDocumentCount();
+            if (estimate > 0) {
+                const newUser = new User({ email, mName, password, type });
+                await newUser.save();
+                res.json({ success: true, message: 'Account created successfully', userData: newUser });
+            } else {
+                const newUser = new User({ email, mName, password, type });
+                await newUser.save();
+                const newAdmin = new Admin({ email, mName, type: 'main' });
+                await newAdmin.save();
+                res.json({ success: true, message: 'Account created successfully', userData: newUser });
+            }
+        } else {
+            return res.json({ success: true, message: 'SignIn Successful', userData: user });
+        }
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Internal Server Error' });
     }
 
 });
@@ -563,7 +597,7 @@ app.post('/api/paypal', async (req, res) => {
 app.post('/api/subscriptiondetail', async (req, res) => {
 
     try {
-        const { uid } = req.body;
+        const { uid, email } = req.body;
 
         const userDetails = await Subscription.findOne({ user: uid });
         if (userDetails.method === 'stripe') {
@@ -585,7 +619,13 @@ app.post('/api/subscriptiondetail', async (req, res) => {
             });
             const session = await response.json();
             res.json({ session: session, method: userDetails.method });
-        } else if (userDetails.method === 'paystack') {
+        }
+        else if (userDetails.method === 'flutterwave') {
+            const payload = { "email": email };
+            const response = await flw.Subscription.get(payload);
+            res.json({ session: response['data'][0], method: userDetails.method });
+        }
+        else if (userDetails.method === 'paystack') {
             const authorization = `Bearer ${process.env.PAYSTACK_SECRET_KEY}`;
             const response = await axios.get(`https://api.paystack.co/subscription/${userDetails.subscriberId}`, {
                 headers: {
@@ -605,7 +645,6 @@ app.post('/api/subscriptiondetail', async (req, res) => {
             res.json({ session: subscriptionDetails, method: userDetails.method });
         }
         else {
-console.log('raz')
             const YOUR_KEY_ID = process.env.RAZORPAY_KEY_ID;
             const YOUR_KEY_SECRET = process.env.RAZORPAY_KEY_SECRET;
             const SUBSCRIPTION_ID = userDetails.subscription;
@@ -711,13 +750,14 @@ app.post('/api/downloadreceipt', async (req, res) => {
 //SEND RECEIPT
 app.post('/api/sendreceipt', async (req, res) => {
     const { html, email, plan, subscriberId, user, method, subscription } = req.body;
-
+    console.log(subscriberId, subscription);
     const existingSubscription = await Subscription.findOne({ user: user });
     if (existingSubscription) {
         //DO NOTHING
     } else {
         const newSub = new Subscription({ user, subscription, subscriberId, plan, method });
         await newSub.save();
+        console.log(newSub);
     }
 
     const transporter = nodemailer.createTransport({
@@ -1717,7 +1757,6 @@ app.post('/api/paystackfetch', async (req, res) => {
                         { _id: uid },
                         { $set: { type: plan } }
                     ).then(async result => {
-                        console.log(subscriptionDetails)
                         res.json({ details: subscriptionDetails });
                     }).catch(error => {
                         res.status(500).json({ success: false, message: 'Internal server error' });
@@ -1827,6 +1866,87 @@ app.post('/api/paystackcancel', async (req, res) => {
 
 });
 
+
+//FLUTTERWAVE PAYMENT
+app.post('/api/flutterwavecancel', async (req, res) => {
+    const { code, token, email } = req.body;
+
+    const payload = { "id": code };
+    const response = await flw.Subscription.cancel(payload)
+    if (response) {
+        const subscriptionDetails = await Subscription.findOne({ subscriberId: token });
+        const userId = subscriptionDetails.user;
+
+        await User.findOneAndUpdate(
+            { _id: userId },
+            { $set: { type: 'free' } }
+        );
+
+        const userDetails = await User.findOne({ _id: userId });
+        await Subscription.findOneAndDelete({ subscriberId: token });
+
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            service: 'gmail',
+            secure: true,
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD,
+            },
+        });
+
+        const Reactivate = process.env.WEBSITE_URL + "/pricing";
+
+        const mailOptions = {
+            from: process.env.EMAIL,
+            to: email,
+            subject: `${userDetails.mName} Your Subscription Plan Has Been Cancelled`,
+            html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+                <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+                <html lang="en">
+                
+                  <head></head>
+                 <div id="__react-email-preview" style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0">Subscription Cancelled<div> ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿ ‌​‍‎‏﻿</div>
+                 </div>
+                
+                  <body style="margin-left:auto;margin-right:auto;margin-top:auto;margin-bottom:auto;background-color:rgb(255,255,255);font-family:ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, &quot;Noto Sans&quot;, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;, &quot;Noto Color Emoji&quot;">
+                    <table align="center" role="presentation" cellSpacing="0" cellPadding="0" border="0" width="100%" style="max-width:37.5em;margin-left:auto;margin-right:auto;margin-top:40px;margin-bottom:40px;width:465px;border-radius:0.25rem;border-width:1px;border-style:solid;border-color:rgb(234,234,234);padding:20px">
+                      <tr style="width:100%">
+                        <td>
+                          <table align="center" border="0" cellPadding="0" cellSpacing="0" role="presentation" width="100%" style="margin-top:32px">
+                            <tbody>
+                              <tr>
+                                <td><img alt="Vercel" src="${process.env.LOGO}" width="40" height="37" style="display:block;outline:none;border:none;text-decoration:none;margin-left:auto;margin-right:auto;margin-top:0px;margin-bottom:0px" /></td>
+                              </tr>
+                            </tbody>
+                          </table>
+                          <h1 style="margin-left:0px;margin-right:0px;margin-top:30px;margin-bottom:30px;padding:0px;text-align:center;font-size:24px;font-weight:400;color:rgb(0,0,0)">Subscription Cancelled</h1>
+                          <p style="font-size:14px;line-height:24px;margin:16px 0;color:rgb(0,0,0)">${userDetails.mName}, your subscription plan has been Cancelled. Reactivate your plan by clicking on the button below.</p>
+                          <table align="center" border="0" cellPadding="0" cellSpacing="0" role="presentation" width="100%" style="margin-bottom:32px;margin-top:32px;text-align:center">
+                               <tbody>
+                                  <tr>
+                                    <td><a href="${Reactivate}" target="_blank" style="p-x:20px;p-y:12px;line-height:100%;text-decoration:none;display:inline-block;max-width:100%;padding:12px 20px;border-radius:0.25rem;background-color:rgb(0,0,0);text-align:center;font-size:12px;font-weight:600;color:rgb(255,255,255);text-decoration-line:none"><span></span><span style="p-x:20px;p-y:12px;max-width:100%;display:inline-block;line-height:120%;text-decoration:none;text-transform:none;mso-padding-alt:0px;mso-text-raise:9px"</span><span>Reactivate</span></a></td>
+                                  </tr>
+                                </tbody>
+                          </table>
+                          <p style="font-size:14px;line-height:24px;margin:16px 0;color:rgb(0,0,0)">Best,<p target="_blank" style="color:rgb(0,0,0);text-decoration:none;text-decoration-line:none">The <strong>${process.env.COMPANY}</strong> Team</p></p>
+                          </td>
+                      </tr>
+                    </table>
+                  </body>
+                
+                </html>`,
+        };
+
+        await transporter.sendMail(mailOptions);
+        res.json({ success: true, message: '' });
+    } else {
+        res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+});
+
+
 //CHAT
 app.post('/api/chat', async (req, res) => {
     const receivedData = req.body;
@@ -1867,6 +1987,42 @@ app.post('/api/chat', async (req, res) => {
         res.status(500).json({ success: false, message: 'Internal server error' });
     })
 
+});
+
+
+//FLUTTERWAVE GET DETAILS
+app.post('/api/flutterdetails', async (req, res) => {
+    const { email, uid, plan } = req.body;
+    try {
+        let cost = 0;
+        if (plan === process.env.MONTH_TYPE) {
+            cost = process.env.MONTH_COST
+        } else {
+            cost = process.env.YEAR_COST
+        }
+        cost = cost / 4;
+
+        await Admin.findOneAndUpdate(
+            { type: 'main' },
+            { $inc: { total: cost } }
+        );
+
+        await User.findOneAndUpdate(
+            { _id: uid },
+            { $set: { type: plan } }
+        ).then(async result => {
+
+            const payload = { "email": email };
+            const response = await flw.Subscription.get(payload);
+
+            res.send(response['data'][0]);
+        }).catch(error => {
+
+            res.status(500).json({ success: false, message: 'Internal server error' });
+        })
+    } catch (error) {
+
+    }
 });
 
 
