6a192e44651c0ba4261750cbf1c18a647a9572f1
Varun Reddy <136375960+varunx007@users.noreply.github.com>
2024-09-10 19:46:43 +0530

    slideOne.js

--- src/components/landing/slideOne.js
+++ src/components/landing/slideOne.js
@@ -17,11 +17,10 @@ const SlideOne = () => {
     return (
         <div className="flex flex-col items-center dark:bg-black">
 
-            <h1 className="text-6xl font-black mt-20 max-md:text-3xl dark:text-white">Ai Course Generator</h1>
+            <h1 className="text-6xl font-black mt-20 max-md:text-3xl dark:text-white">Personalized AI Learning</h1>
 
             <p className="text-center text-black mt-6 max-w-2xl font-medium max-md:text-xs dark:text-white">
-                Revolutionize your learning journey with our AI Course Generator SaaS
-                Effortlessly create engaging and personalized courses tailored to your needs
+                Transform your learning experience with our AI-powered platform. Easily access engaging, personalized content tailored to your unique learning needs.
             </p>
 
             <div className="flex space-x-4 mb-4 mt-6">
