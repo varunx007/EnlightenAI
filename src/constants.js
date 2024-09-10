364a60fa699ad8a05dd2bc785c675f5d0057e883
Varun Reddy <136375960+varunx007@users.noreply.github.com>
2024-09-10 19:40:29 +0530

    constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 //BRAND
-export const name = 'EnlightenAI';
-export const company = 'EnlightenAI';
+export const name = 'AdeptLearn';
+export const company = 'AdeptLearn';
 export const websiteURL = 'http://localhost:3000';
 export const serverURL = 'http://localhost:5000';
 export const logo = 'https://firebasestorage.googleapis.com/v0/b/enlightenai.appspot.com/o/EnlightenAI%20logo.png?alt=media&token=79f4df7d-6883-4e26-b257-0bc598984f81';
@@ -18,18 +18,18 @@ export const FreeTime = '';
 
 //MONTHLY 
 export const MonthType = 'Monthly Plan';
-export const MonthCost = 9;
+export const MonthCost = 0;
 export const MonthTime = 'month';
 
 //YEARLY 
 export const YearType = 'Yearly Plan';
-export const YearCost = 99;
+export const YearCost = 0;
 export const YearTime = 'year';
 
 //TESTIMONIAL
-export const review = "The AI Course Generator revolutionized my content creation process, providing accurate and relevant topics effortlessly. It's a time-saving powerhouse that enhances the quality and relevance of my courses. A must-have tool for educators seeking efficiency and impactful online learning experiences.";
-export const from = "Anam Meena Sharif";
-export const profession = 'CFO at Spacester';
+export const review = "The Personalized AI Learning Platform transformed my learning process, delivering accurate and relevant content effortlessly. It’s a time-saving tool that enhances the quality of my learning experience. A must-have platform for anyone seeking efficiency and personalized knowledge tailored to their needs.";
+export const from = "AdeptLearn";
+export const profession = '';
 export const photoURL = 'https://play-lh.googleusercontent.com/sV_ffBmBJt_je4RZHnfaCfcnL-Hy6C14Iol7H5EMj9fzI2GDOonuojdn5t9p6n9IAX8j';
 
 //PAYPAL
